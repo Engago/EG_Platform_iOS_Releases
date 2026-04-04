c165ab1969898de2617b19503c4f005c2017e164
Brian Thorp <brian@graveltree.com>
2026-04-04 14:24:02 -0400

    Release 1.0.4

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "EGPlatform",
-            url: "https://github.com/Engago/EG_Platform_iOS_Releases/releases/download/1.0.3/EGPlatform.xcframework.zip",
-            checksum: "cac46a38c240d8bc5a9bf900c80701e4e8c6f927e9cd8877cd5b9d4416cc92d6"
+            url: "https://github.com/Engago/EG_Platform_iOS_Releases/releases/download/1.0.4/EGPlatform.xcframework.zip",
+            checksum: "4325b280dfdfee0c034f5b0ebca75d6fd66b4f657d167593ff0c9102b5cea650"
         )
     ]
 )
